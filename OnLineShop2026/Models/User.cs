679679c9361550245e07d161844cde2d838794cc
alinabzykova <bzykovaaa@gmail.com>
2026-03-31 19:27:37 +0300

    Подзадача 1

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,13 @@
+namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string AvatarUrl { get; set; } = "/images/default.png";
+    }
+}
