cc35baf5a20dad78443479fa57158a1a0b359e2d
alinabzykova <bzykovaaa@gmail.com>
2026-03-31 19:36:13 +0300

    Подзадача 2

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = new User
+            {
+                FullName = "Алина Бзыкова",
+                Email = "bzykovaaa@gmail.com"
+            };
+
+            return View(user);
+        }
+    }
+}
