d01e775d1324e1bdb23af7840edf7656ea37a87e
alinabzykova <bzykovaaa@gmail.com>
2026-04-07 18:24:44 +0300

    Переделала подзадачу 1

--- OnLineShop2026/Models/User.cs
+++ OnLineShop2026/Models/User.cs
@@ -2,12 +2,32 @@ namespace OnLineShop2026.Models
 {
     public class User
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        public string FullName { get; set; }
-
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
+        public string? Avatar { get; set; }
 
-        public string AvatarUrl { get; set; } = "/images/default.jpg";
+        public string FullName => FirstName + " " + LastName;
+
+        private const string DefaultAvatar = "/images/defaultImage.jpg";
+
+        public User()
+        {
+            Id = Guid.NewGuid();
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            Avatar = DefaultAvatar;
+        }
+
+        public User(string firstName, string lastName, string email, string? avatar = null)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Avatar = avatar ?? DefaultAvatar;
+        }
     }
 }
